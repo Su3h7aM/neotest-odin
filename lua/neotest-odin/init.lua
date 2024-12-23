5a4b4bf770318b826023a41db00c0ba49ef14316
Su3h7aM <matheusgasource@gmail.com>
2024-12-23 15:28:12 -0300

    feat(adapter): a cleanup to have a minimally functional version
    Fall back to using only individual tests, this makes the adapter functional, but there may be some performance issues.

--- lua/neotest-odin/init.lua
+++ lua/neotest-odin/init.lua
@@ -1,5 +1,4 @@
 local lib = require("neotest.lib")
-local async = require("neotest.async")
 
 local odin = {}
 
@@ -9,12 +8,16 @@ odin.adapter = {
 }
 
 odin._test_query = [[
+    ;; query package
+    (package_declaration
+        (identifier) @package.name ) @package.definition
+    
     ;; query for test procedures
-    ((procedure_declaration
+    (procedure_declaration
         (attributes
             (attribute
                 (identifier) @attribute (#eq? @attribute "test")))
-        (identifier) @test.name )) @test.definition
+        (identifier) @test.name ) @test.definition
 ]]
 
 ---@async
@@ -62,6 +65,7 @@ end
 ---@param file_path string Absolute file path
 ---@return neotest.Tree | nil
 function odin.adapter.discover_positions(file_path)
+	--TODO: stop using the builtin parse_positions(), build a custom for generate a real tree
 	local positions = lib.treesitter.parse_positions(file_path, odin._test_query, {})
 
 	return positions
@@ -71,30 +75,54 @@ end
 ---@return nil | neotest.RunSpec | neotest.RunSpec[]
 function odin.adapter.build_spec(args)
 	local position = args.tree:data()
-	local cwd = vim.fn.getcwd()
 	local head_path = vim.fn.fnamemodify(position.path, ":h")
 
-	local command = nil
-	local flags =
-		" -define:ODIN_TEST_LOG_LEVEL=warning -define:ODIN_TEST_TRACK_MEMORY=false -define:ODIN_TEST_FANCY=false"
+	local flags = ""
+	--local flags = " -define:ODIN_TEST_LOG_LEVEL=warning -define:ODIN_TEST_TRACK_MEMORY=false -define:ODIN_TEST_FANCY=false"
 
-	local results = async.fn.tempname()
+	local specs = {}
 
-	if position.type == "test" or position.type == "namespace" then
-		command = "odin test " .. head_path .. " -define:ODIN_TEST_NAMES=main." .. position.name .. flags
+	if position.type == "test" then
+		table.insert(specs, {
+			command = "odin test " .. head_path .. " -define:ODIN_TEST_NAMES=main." .. position.name .. flags,
+			context = {
+				name = position.name,
+				id = position.id,
+			},
+		})
 	elseif position.type == "file" then
-		command = "odin test " .. head_path .. flags
+		return nil
+		-- list = args.tree:to_list()
+		--
+		-- for _, item in ipairs(list) do
+		-- 	if #item == 0 then
+		-- 		goto continue
+		-- 	end
+		--
+		-- 	if item[1].type == "test" then
+		-- 		table.insert(specs, {
+		-- 			command = "odin test " .. head_path .. " -define:ODIN_TEST_NAMES=main." .. item[1].name,
+		-- 			context = {
+		-- 				name = item[1].name,
+		-- 				id = item[1].id,
+		-- 			},
+		-- 		})
+		-- 	end
+		--
+		-- 	::continue::
+		-- end
 	elseif position.type == "dir" then
-		command = "odin test " .. head_path .. flags
+		return nil
+		-- table.insert(specs, {
+		-- 	command = "odin test " .. position.path .. flags,
+		-- 	context = {
+		-- 		name = position.name,
+		-- 		id = position.id,
+		-- 	},
+		-- })
 	end
 
-	return {
-		command = command .. " 2>" .. results,
-		context = {
-			results = results,
-		},
-		cwd = cwd,
-	}
+	return specs
 end
 
 ---@async
@@ -103,28 +131,26 @@ end
 ---@param tree neotest.Tree
 ---@return table<string, neotest.Result>
 function odin.adapter.results(spec, result, tree)
-	local position = tree:data()
-	local results = {}
-	local file = assert(io.open(spec.context.results))
-	local line = file:read("l")
+	-- local output = assert(io.open(result.output))
+	-- local line = output:read("*a")
+	-- output:close()
 
-	while line do
-		local passed = string.match(line, "successful")
-		if passed ~= nil then
-			results[position.path .. "::" .. position.name] = { status = "passed" }
-		end
-
-		local failed = string.match(line, "failed")
-		if failed ~= nil then
-			results[position.path .. "::" .. position.name] = { status = "failed" }
-		end
-
-		line = file:read("l")
+	local status = "failed"
+	if result.code == 0 then
+		status = "passed"
 	end
 
-	if file then
-		file:close()
-	end
+	--TODO: parse result.output for generate a short version and errors messages
+	local results = {
+		[spec.context.id] = {
+			status = status,
+			output = result.output,
+			--          short = "testando: ola",
+			-- errors = {
+			-- 	{ message = "erro", line = 5 },
+			-- },
+		},
+	}
 
 	return results
 end
